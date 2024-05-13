c58b813c5723169d13fac194a2c3561726a519f9
falendikategar <falendikapratama@gmail.com>
2024-05-13 19:53:16 +0700

    feat(Register): Update ragister dan Penambahan CRUD Product dan Service

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductCollection;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 use Inertia\Inertia;
 
 class ProductController extends Controller
@@ -18,5 +20,89 @@ class ProductController extends Controller
             'title' => 'Products Management',
             'products' => $products
         ]);
+    }
+
+    // Tampil Halaman Create Product
+    public function createProductPage()
+    {
+        return Inertia::render('Admin/Products/CreateProduct', [
+            'title' => 'Create Product'
+        ]);
+    }
+
+    // Create Product
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name_product' => 'required|string|max:255',
+            'deskripsi_product' => 'required|text',
+            'price_product' => 'required|string|max:255',
+            'image_product' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'stock_product' => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $product = new Product;
+        $product->name_product = $request->name_product;
+        $product->deskripsi_product = $request->deskripsi_product;
+        $product->price_product = $request->price_product;
+        $product->stock_product = $request->stock_product;
+
+        if ($request->hasFile('image_product')) {
+            $imageName = uniqid('product_') . '.' . $request->image_product->getClientOriginalExtension();
+            $path = $request->image_product->storeAs('images/products', $imageName, 'public');
+            if (!$path) {
+                return response()->json(['error' => 'Failed to upload image'], 500);
+            }
+            $product->image_product = $path;
+        }
+
+        $product->save();
+
+        return redirect()->route('admin.products');
+    }
+
+    // Tampil Halaman Update Product
+    public function updateProductPage(Product $product)
+    {
+        $productData = Product::find($product->product_id);
+        return Inertia::render('Admin/Products/UpdateProduct', [
+            'title' => 'Update Product',
+            'product' => $productData
+        ]);
+    }
+
+    // Update Product
+    public function update(Request $request, Product $product)
+    {
+        if ($request->hasFile('image_product')) {
+            $imageName = uniqid('product_') . '.' . $request->file('image_product')->getClientOriginalExtension();
+            $path = $request->file('image_product')->storeAs('images/products', $imageName, 'public');
+            if (!$path) {
+                return response()->json(['error' => 'Failed to upload image'], 500);
+            }
+            Storage::disk('public')->delete($product->image_product);
+            $product->image_product = $path;
+        }
+
+        $product->update([
+            'name_product' => $request->name_product,
+            'deskripsi_product' => $request->deskripsi_product,
+            'price_product' => $request->price_product,
+            'stock_product' => $request->stock_product,
+        ]);
+
+        return redirect()->route('admin.products');
+    }
+
+    // Delete Product
+    public function destroy(Product $product)
+    {
+        Storage::disk('public')->delete($product->image_product);
+        $product->delete();
+        return redirect()->route('admin.products');
     }
 }
--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\RoleUser;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -10,6 +11,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -31,6 +33,8 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        $user_id = 'USR-' . date('ymdhis');
+
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
@@ -38,15 +42,23 @@ class RegisteredUserController extends Controller
         ]);
 
         $user = User::create([
+            'user_id' => $user_id,
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
+        $role_user = new RoleUser();
+        $role_user->role_id = 3;
+        $role_user->user_id = $user_id;
+
+        $role_user->save();
+
         event(new Registered($user));
 
         Auth::login($user);
 
-        return redirect(RouteServiceProvider::HOME);
+        return redirect()->route('owner.dashboard');
+        // return redirect(RouteServiceProvider::HOME);
     }
 }
