e687071c96d98a6bd017084635ad7a2c3ce04048
rahmadnasirin <59747382+rahmad180601@users.noreply.github.com>
2024-05-30 23:37:09 +0700

    transaction detail show

--- app/Http/Resources/TransactionSuperAdminResource.php
+++ app/Http/Resources/TransactionSuperAdminResource.php
@@ -14,6 +14,38 @@ class TransactionSuperAdminResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'invoice' => $this->invoice,
+            'date_transaction' => $this->date_transaction,
+            'subtotal' => $this->subtotal,
+            'status_payment' => $this->status_payment,
+            'user' => [
+                'user_id' => $this->user->user_id,
+                'name' => $this->user->name,
+                'email' => $this->user->email,
+            ],
+            'appointment' => [
+                'appointment_id' => $this->appoitment->appointmen_id,
+                'pet_id' => $this->appoitment->pet_id,
+                'pet_name' => $this->appoitment->pet->name,
+                'doctor_id' => $this->appoitment->docter_id,
+                'doctor_name' => $this->appoitment->docter->name,
+                'status' => $this->appoitment->status,
+                'jadwal' => $this->appoitment->jadwal,
+                'description' => $this->appoitment->description,
+                'date_appointments' => $this->appoitment->date_appointments,
+            ],
+            'details' => $this->details->map(function ($detail) {
+                return [
+                    'transaction_id' => $detail->transaction_id,
+                    'product_id' => $detail->product_id,
+                    'service_id' => $detail->service_id,
+                    'quantity' => $detail->quantity,
+                    'harga_product' => $detail->harga_product,
+                    'harga_service' => $detail->harga_service,
+                ];
+            })->toArray(),
+        ];
     }
 }
